1914bfa915819faf7ccfb1d6102528e74a4d3b8b
Moses Burkhart <moses.burkhart1@gmail.com>
2022-06-12 10:04:58 -0400

    linked app views to template

--- sample_app/views.py
+++ sample_app/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http.response import HttpResponse
 
 def home_view(request):
-    return HttpResponse('HOME PAGE')
+    return render(request, 'sample_app/home.html')
