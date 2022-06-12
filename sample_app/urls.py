67a4700f5d03feb1d14ea935744b6999438198bf
Moses Burkhart <moses.burkhart1@gmail.com>
2022-06-12 11:11:48 -0400

    added link from one html page to another

--- sample_app/urls.py
+++ sample_app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
+#register app namespace
+#URL NAMES
+app_name = 'sample_app'
+
 urlpatterns = [
-    path('', views.home_view),
-    path('variables/', views.variables_view)
+    path('', views.home_view, name="home"),
+    path('variables/', views.variables_view, name="variables")
 ]
--- sample_app/views.py
+++ sample_app/views.py
@@ -6,12 +6,12 @@ def home_view(request):
 
 def variables_view(request):
 
-    my_var = {
-        'first_name': 'moses',
-        'last_name': 'burkhart',
-        'some_list': [1,2,3],
-        'user_logged_in': False,
-        'some_dict': {'inside_key': 'inside_value'}
-    }
+    # my_var = {
+    #     'first_name': 'moses',
+    #     'last_name': 'burkhart',
+    #     'some_list': [1,2,3],
+    #     'user_logged_in': False,
+    #     'some_dict': {'inside_key': 'inside_value'}
+    # }
 
-    return render(request, 'sample_app/variables.html', context=my_var)
+    return render(request, 'sample_app/variables.html')
